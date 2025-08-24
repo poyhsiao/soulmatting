459587aaf7b0c082041e67b677db26b2e2b9f820
Kim Hsiao <white.shopping@gmail.com>
2025-08-24 12:20:17 +0800

    fix: Add ESLint configuration to ignore generated Prisma client files
    - Created .eslintrc.js for database package to ignore src/generated/**/*
    - Created root .eslintignore to globally ignore generated files and build outputs
    - This should resolve ESLint errors in CI/CD pipeline for generated Prisma client

---
+++ packages/database/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['../../.eslintrc.js'],
+  ignorePatterns: [
+    'src/generated/**/*',
+    'dist/**/*',
+    'node_modules/**/*',
+    '*.d.ts'
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+};
