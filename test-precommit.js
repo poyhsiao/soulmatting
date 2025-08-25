381d041a90a7489f1011edd34e1c3912a921c467
Kim Hsiao <white.shopping@gmail.com>
2025-08-25 11:01:52 +0800

    test: add test file for pre-commit hook validation

---
+++ test-precommit.js
@@ -0,0 +1,8 @@
+// Test file for pre-commit hook validation
+const testFunction = () => {
+  // eslint-disable-next-line no-console
+  console.log('This file has been formatted');
+  return 'success';
+};
+
+module.exports = testFunction;
